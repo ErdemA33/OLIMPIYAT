f6a6181c95d4fd22e2194ce3baf965a6b38c9cfd
Erdem AYDIN <erdemaydinn32@gmail.com>
2025-11-09 10:39:55 +0300

    Add files via upload

---
+++ p2_67.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	int x;
+	int i,max = 1;
+
+	for(i=1;i <= 750; i++){
+
+
+		if(750%i == 1881 % i && 750%i == 3389%i && 750%i == 5651%i)x = i;
+		
+
+
+	}
+	printf("%d",x);
+
+}
+
+
